330f1e98963c78e858898c8b3c6783169eed05e9
unknown <ismaelsuring@gmail.com>
2026-01-24 12:24:42 -0800

    Updated climber, added limits, and to not go over level 3

--- src/main/java/frc/robot/subsystems/Climber.java
+++ src/main/java/frc/robot/subsystems/Climber.java
@@ -8,6 +8,8 @@ import com.revrobotics.ResetMode;
 import com.revrobotics.spark.SparkMax;
 import com.revrobotics.spark.SparkLowLevel.MotorType;
 import com.revrobotics.spark.config.SparkMaxConfig;
+\
+
 
 import edu.wpi.first.math.controller.PIDController;
 import edu.wpi.first.wpilibj.DigitalInput;
