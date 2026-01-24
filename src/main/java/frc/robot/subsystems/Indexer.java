c57f892762c993b5e17112ae2717c13ef745d76f
unknown <ismaelsuring@gmail.com>
2026-01-23 19:51:13 -0800

    indexer command created

--- src/main/java/frc/robot/subsystems/Indexer.java
+++ src/main/java/frc/robot/subsystems/Indexer.java
@@ -3,15 +3,18 @@ package frc.robot.subsystems;
 import com.revrobotics.spark.SparkMax;
 import com.revrobotics.spark.SparkLowLevel.MotorType;
 
+import edu.wpi.first.wpilibj.motorcontrol.Spark;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Constants;
 
 public class Indexer extends SubsystemBase{
 
-   private final SparkMax indexMotor = new SparkMax(Constants.indexerMotorID, MotorType.kBrushless);
-   
+   private final SparkMax indexMotor; 
+
+
    public Indexer() {
+   indexMotor = new SparkMax(Constants.indexerMotorID, MotorType.kBrushless);
 
    }
 
