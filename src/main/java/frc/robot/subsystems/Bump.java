131b0d10c787f07ee00b0d7f097dcd8a35fb2cb9
Jackson Tripp <jackson.ltyi@gmail.com>
2026-01-28 19:44:01 -0800

    added right libraries this time

--- src/main/java/frc/robot/subsystems/Bump.java
+++ src/main/java/frc/robot/subsystems/Bump.java
@@ -1,37 +1,23 @@
 package frc.robot.subsystems;
 
-import com.ctre.phoenix6.Orchestra;
-import com.pathplanner.lib.auto.AutoBuilder;
-import com.pathplanner.lib.config.PIDConstants;
-import com.pathplanner.lib.config.RobotConfig;
-import com.pathplanner.lib.controllers.PPHolonomicDriveController;
-import com.reduxrobotics.sensors.canandgyro.Canandgyro;
+import com.revrobotics.spark.SparkMax;
+import com.revrobotics.spark.SparkLowLevel.MotorType;
 
-import swervelib.SwerveModule;
-import edu.wpi.first.math.estimator.SwerveDrivePoseEstimator;
-import edu.wpi.first.math.filter.SlewRateLimiter;
-import edu.wpi.first.math.geometry.Pose2d;
-import edu.wpi.first.math.geometry.Rotation2d;
-import edu.wpi.first.math.geometry.Translation2d;
-import edu.wpi.first.math.kinematics.ChassisSpeeds;
-import edu.wpi.first.math.kinematics.SwerveDriveKinematics;
-import edu.wpi.first.math.kinematics.SwerveDriveOdometry;
-import edu.wpi.first.math.kinematics.SwerveModulePosition;
-import edu.wpi.first.math.kinematics.SwerveModuleState;
-import edu.wpi.first.networktables.BooleanEntry;
-import edu.wpi.first.networktables.NetworkTableInstance;
-import edu.wpi.first.util.sendable.Sendable;
-import edu.wpi.first.util.sendable.SendableBuilder;
-import edu.wpi.first.wpilibj.DriverStation;
-import edu.wpi.first.wpilibj.smartdashboard.Field2d;
-import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import edu.wpi.first.wpilibj.motorcontrol.Spark;
+import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
-import frc.robot.Constants.DriveConstants;
-import frc.robot.Constants.DriveConstants.ModuleLocations;
-import frc.robot.Constants.DriveConstants.SwerveModules;
-import frc.robot.subsystems.vision.CameraBlock;
+import frc.robot.Constants;
 
 public class Bump extends SubsystemBase {
+
+        //note to self: add the gyro and use to detect when robot is going over bump and speed up motors to give boost
+
+
+
+
+
+
+
 
     public Bump() {
 
