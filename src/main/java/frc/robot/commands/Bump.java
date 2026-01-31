32dbcc66cf361e6edf1b53b602872b468d5bb182
Jackson Tripp <jackson.ltyi@gmail.com>
2026-01-30 19:03:13 -0800

    made the robot lock at 45 degrees in command

--- src/main/java/frc/robot/commands/Bump.java
+++ src/main/java/frc/robot/commands/Bump.java
@@ -1,30 +1,76 @@
-package frc.robot.subsystems;
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.commands;
+
+import java.util.function.DoubleSupplier;
+import java.util.function.Supplier;
+
+import edu.wpi.first.wpilibj2.command.Command;
 import frc.robot.subsystems.Drivebase;
 
-import com.revrobotics.spark.SparkMax;
-import com.revrobotics.spark.SparkLowLevel.MotorType;
+import edu.wpi.first.math.controller.ProfiledPIDController;
 
-import edu.wpi.first.wpilibj.motorcontrol.Spark;
-import edu.wpi.first.wpilibj2.command.Command;
-import edu.wpi.first.wpilibj2.command.SubsystemBase;
-import frc.robot.Constants;
-import com.reduxrobotics.sensors.canandgyro.Canandgyro;
 
-public class Bump extends SubsystemBase {
+public class Drive extends Command {
 
-    //note to self: add the gyro and use to detect when robot is going over bump and speed up motors to give boost
+  private final Drivebase drivebase;
+  private final Supplier<double[]> speedXY;
+  private final DoubleSupplier rot;
+  private final ProfiledPIDController thetaController = new ProfiledPIDController(1.0, 0.0, 0.0, TrapezoidProfile.Constraints(10, 10));
 
-    private Drivebase drivebase;
-    private Canandgyro gyro;
+  /** Creates a new Drive. */
+  public Drive(Drivebase drivebase, Supplier<double[]> speedXY, DoubleSupplier rot) {
+    this.drivebase = drivebase;
+    this.speedXY = speedXY;
+    this.rot = rot;
 
-    public Bump(Drivebase m_drivebase, Canandgyro m_gyro) {
+    // Use addRequirements() here to declare subsystem dependencies.
+    thetaController.setTolerance(Math.toRadians(2));
 
-        this.m_drivebase = drivebase;
-        this.m_gyro = gyro;
+    addRequirements(this.drivebase);
+  }
+
+  // Called when the command is initially scheduled.
+  @Override
+  public void initialize() {
+  }
+
+  // Called every time the scheduler runs while the command is scheduled.
+  @Override
+  public void execute() {
+    var xy = speedXY.get();
+    double output;
+    boolean isLocking = false;
+    boolean isAtAngleThreshold = false;
+
+    if (isAtAngleThreshold == true) {
+      isLocking = true;
+    } else {
+      isLocking = false;
     }
 
-    
-    
+    if (isLocking) {
+        double currentAngle = drivebase.getPose().getRotation().getRadians();
+        double goalAngle = Math.toRadians(45);
+        output = thetaController.calculate(currentAngle, goalAngle);
+    } else {
+        
+        output = rot.getAsDouble
+    }
+
+    drivebase.defaultDrive(-xy[1], -xy[0], output);
+  }
+
+  // Called once the command ends or is interrupted.
+  @Override
+  public void end(boolean interrupted) {
+  }
+
+  // Returns true when the command should end.
+  @Override
+  public boolean isFinished() {
+    return false;
+  }
 }
-
-
