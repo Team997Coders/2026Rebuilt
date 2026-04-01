7c8a8e562f690b54eb7abfc80f3b1b61fdc3fa53
unknown <ismaelsuring@gmail.com>
2026-03-31 18:43:16 -0700

    stuff from end of cais

--- src/main/java/frc/robot/subsystems/IntakeSpinny.java
+++ src/main/java/frc/robot/subsystems/IntakeSpinny.java
@@ -32,7 +32,7 @@ public class IntakeSpinny extends SubsystemBase {
 
     public void output()
     {
-        spinMotor.set(-1.0);
+        spinMotor.set(-0.997);    
     }
 
     public Command intakeFuel()
